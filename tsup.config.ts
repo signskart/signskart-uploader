cf461e85cd329845d1faef37b152a49b8cccde2c
ibreakloops <rajansharma46@gmail.com>
2026-03-09 18:18:27 -0700

    feat(ts): issue

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,37 +1,13 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
 
-export default defineConfig([
-    {
-        entry: {
-            index: "src/index.ts",
-            "server/index": "src/server/index.ts",
-        },
-        format: ["cjs", "esm"],
-        dts: true,
-        sourcemap: true,
-        splitting: false,
-        clean: true,
-        treeshake: true,
-        target: "es2020",
-        platform: "node",
-    },
-
-    // Explicit .mjs for Node.js ESM
-    {
-        entry: {
-            index: "src/index.ts",
-            "server/index": "src/server/index.ts",
-        },
-        format: ["esm"],
-        dts: false,
-        sourcemap: true,
-        splitting: false,
-        treeshake: true,
-        target: "es2020",
-        platform: "node",
-        outDir: "dist-mjs",
-        esbuildOptions(options) {
-            options.outExtension = { ".js": ".mjs" };
-        },
-    },
-]);
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm', 'cjs'],
+    dts: true,
+    sourcemap: true,
+    splitting: false,
+    treeshake: true,
+    clean: true,
+    target: 'es2020',
+    platform: 'neutral',
+});
